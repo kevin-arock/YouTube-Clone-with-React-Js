8c60003fe2568b87bec0b44d293045b7e27b9cde
kevin-arock <kevinstandlee2002@gmail.com>
2024-08-31 17:32:37 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Head } from "./components/Head";
+import { Body } from "./components/Body";
+
 
 function App() {
   return (
     <div >
-      <h1 className="bg-red-900">hello</h1>
+      <Head/>
+      <Body/>
     </div>
   );
 }
